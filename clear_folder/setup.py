732c778c6ec9f98153366a0126cd990b0449bc54
AlexM-resource <mixailovaa@gmail.com>
2023-05-27 21:14:33 +0300

    Update setup.py

--- clear_folder/setup.py
+++ clear_folder/setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+From setuptools import setup, find_namespace_packages
+
 setup(
     name='clean',
     version='0.1',
-    description='sort of files in folder',
+    description='Sort file in folder',
     url='http://github.com/dummy_user/useful',
     author='AlexM',
     author_email='flyingcircus@example.com',
     license='MIT',
-    packages=['useful'])
+    packages=find_namespace_packages(),
+    install_requires=['os','shutil'],
+    entry_points={'console_scripts': ['sortfile = useful.some_code:sort_files']}
 )
