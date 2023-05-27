9d685d68ae85517025d43a1b6466ce7434c7e1c7
AlexM-resource <mixailovaa@gmail.com>
2023-05-27 20:41:30 +0300

    Update setup.py

---
+++ clear_folder/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='clean',
+    version='0.1',
+    description='sort of files in folder',
+    url='http://github.com/dummy_user/useful',
+    author='AlexM',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=['useful'])
+)
